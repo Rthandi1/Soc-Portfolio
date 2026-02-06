a70edb58d8a82ca912629480b3b442066c8001c1
Rthandi1 <thandi.rayman@gmail.com>
2026-02-06 15:48:10 -0800

    #fixes

--- components/FastLane.tsx
+++ components/FastLane.tsx
@@ -1,9 +1,10 @@
 
 import React from 'react';
 
+
 const FastLane: React.FC = () => {
   const links = [
-    { label: 'Resume', url: 'resume\\Rayman Thandi Resume-Cybersecurity.pdf', download: "Rayman-Thandi-Resume.pdf", icon: (
+    { label: 'Resume', url: 'resume\\Rayman%20Thandi%20Resume-Cybersecurity.pdf', download: 'public/resume/Rayman_Thandi_Resume-Cybersecurity.pdf', icon: (
       <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" /></svg>
     ) },
     { label: 'LinkedIn', url: 'https://www.linkedin.com/in/rayman-thandi-847b46196', icon: (
--- components/Header.tsx
+++ components/Header.tsx
@@ -19,9 +19,9 @@ const Header: React.FC = () => {
               Contact
             </a>
             <a 
-              href="resume\Rayman Thandi Resume-Cybersecurity.pdf"
+              href={`/resume/Rayman_Thandi_Resume-Cybersecurity.pdf`}
               className="bg-indigo-600 text-white px-5 py-2.5 rounded-lg font-semibold shadow-sm hover:bg-indigo-700 transition-all flex items-center"
-              download="Rayman-Thandi-Resume.pdf"
+              download
             >
               <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
